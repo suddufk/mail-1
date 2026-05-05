e899d8d597a4032afb115de3845b783e465dd624
shishaosong <41780911+shishaosong@users.noreply.github.com>
2026-05-05 22:37:05 +0800

    Update index.js

--- mail-vue/src/i18n/index.js
+++ mail-vue/src/i18n/index.js
@@ -1,8 +1,11 @@
 import { createI18n } from 'vue-i18n';
 import en from './en.js'
 import zh from './zh.js'
+
 const i18n = createI18n({
     legacy: false,
+    locale: 'zh',          // <--- 添加这一行，强制默认使用中文
+    fallbackLocale: 'en',  // <--- 建议添加这一行，如果中文包里没翻译，就找英文补位
     messages: {
         zh,
         en
